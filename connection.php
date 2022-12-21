c7344bccf2c69d714c807a03418e6d9e6df0cdae
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-12-21 20:24:57 +0100

    update 2

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbName = "login_db";
+
+	//create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbName);
+
+?>
